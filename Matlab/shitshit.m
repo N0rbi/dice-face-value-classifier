f1b4d71008e4540e9938189a2155b767adc6b14f
SzatmariA <ati.szatmari95@gmail.com>
2018-11-17 14:24:54 +0100

    This gives the edges.

--- Matlab/shitshit.m
+++ Matlab/shitshit.m
@@ -1,29 +1,54 @@
-A=imread('dice4.jpg');
+close all;
+A=imread('dice2.jpg');
+figure; imshow(A);
+
+%A = A - sqrt(power(A - mean(A),2));
 %figure; imshow(A);
 B = rgb2gray(A);
-%figure; imshow(B);
-B = im2bw(B, graythresh(B));
 figure; imshow(B);
-B = imcomplement(B);
-figure; imshow(B);
+A = adapthisteq(B);
+figure; imshow(A);
+bw = im2bw(B, 0.3);
+imshow(bw)
+bw = imcomplement(bw);
+bw2 = imfill(bw, 'holes');
+figure; imshow(bw2); title('asd');
 
-%eldetektalas = fspecial('average', [16 1]); 
-%The second argument can be a vector specifying the number of rows and columns in h
-%Probalgatas utan a [16 1] mukszik jol, idk why tho.
-
-%A = imfilter(B, eldetektalas);
-%figure, imshow(A);title('Szurt kép');
-
+B = edge(bw2, 'Canny');
 se = strel('disk',4);
-vizszintes=imerode(B,se);
+B=imdilate(B,se);
+se = strel('disk',3);
+B=imerode(B, se);
+B = bwareaopen(B, 50);
+L = bwlabel(B);
+figure; imshow(B);
+figure; imshow(L == 1)
+title('Object 1')
 
-vizszintes=imerode(vizszintes,se);
-vizszintes=imclose(vizszintes,se);
 
-vizszintes = bwareaopen(vizszintes, 30);
+%B = im2bw(B, graythresh(B));
+%figure; imshow(B);
+%text(5,30,strcat('\color{red}Sum of dots:',num2str(length(B))))
 
-B = bwboundaries(vizszintes);
-figure, imshow(vizszintes);title('Number of dots on the upper surface  ');
+%LB = 20;
+%UB = 50;
+%Iout = xor(bwareaopen(bw,LB),  bwareaopen(bw,UB));
+%figure, imshow(Iout); title('Iout ');
 
-text(5,30,strcat('\color{red}Sum of dots:',num2str(length(B) - 2)))
+
+%figure; imshow(B);
+
+%se = strel('disk',3);
+%vizszintes=imdilate(Iout,se);
+
+%vizszintes=imerode(vizszintes,se);
+%vizszintes=imerode(vizszintes,se);
+%vizszintes=imclose(vizszintes,se);
+
+%vizszintes = bwareaopen(vizszintes, 100);
+%figure, imshow(vizszintes);title('Motherfuck ');
+%B = bwboundaries(vizszintes);
+%figure, imshow(vizszintes); title('Number of dots on the upper surface  ');
+
+%text(5,30,strcat('\color{red}Sum of dots:',num2str(length(B) - 2)))
 
