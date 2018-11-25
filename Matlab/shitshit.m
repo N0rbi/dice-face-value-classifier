cef4c53eabea776694a9f44db96bc6c1eba8f861
SzatmariA <ati.szatmari95@gmail.com>
2018-11-25 14:12:20 +0100

    some changes

--- Matlab/shitshit.m
+++ Matlab/shitshit.m
@@ -1,55 +1,61 @@
 close all;
-A=imread('dice3.jpg');
-figure; imshow(A);
-B = rgb2gray(A);
-figure; imshow(B);
-A = adapthisteq(B);
-figure; imshow(A);
-bw = im2bw(B, 0.3);
+ORIGINAL=imread('dice10.jpg');
+figure; imshow(ORIGINAL); title('Original');
+B = rgb2gray(ORIGINAL);
+bw = im2bw(B);
+figure;
 imshow(bw)
 bw = imcomplement(bw);
 bw2 = imfill(bw, 'holes');
-figure; imshow(bw2); title('asd');
-bw2 = imcomplement(bw2);
+L = bwlabel(bw2);
+
+bw2 = bwareaopen(bw2, 100);
+for k=1:2
+    CC = bwconncomp(bw2)
+    numPixels = cellfun(@numel,CC.PixelIdxList);
+    [biggest,idx] = max(numPixels);
+    bw2(CC.PixelIdxList{idx}) = 0;
+    
+end
+
+se = strel('disk', 4);
+bw2 = imerode(bw2, se);
+bw2 = bwareaopen(bw2, 100);
+bw2 = imerode(bw2, se);
+CC = bwconncomp(bw2);
+numPixels = cellfun(@numel,CC.PixelIdxList);
+[biggest,idx] = max(numPixels);
 figure; imshow(bw2); title('asdasd');
-B = edge(bw2, 'Canny');
-se = strel('disk',4);
-B=imdilate(B,se);
-se = strel('disk',3);
-B=imerode(B, se);
-B = bwareaopen(B, 50);
 
 
+stats = regionprops('table',bw2,'Centroid',...
+    'MajorAxisLength','MinorAxisLength')
+centers = stats.Centroid;
+diameters = mean([stats.MajorAxisLength stats.MinorAxisLength],2);
+radii = diameters/2;
 
-L = bwlabel(B);
-figure; imshow(B);
-%figure; imshow(L == 1)
-%title('Object 1')
+[labeledImage, numberOfObject] = bwlabel(bw2);
+hold on
+viscircles(centers,radii);
+hold off
+[B,L, N, A] = bwboundaries(bw2,'noholes');
+RGB = label2rgb(labeledImage);
+figure
+imshow(RGB);
+figure;
+imshow(ORIGINAL); 
+hold on
+colors=['b' 'g' 'r' 'c' 'm' 'y'];
+for k=1:length(B),
+  boundary = B{k};
+  cidx = mod(k,length(colors))+1;
+  plot(boundary(:,2), boundary(:,1),...
+       colors(cidx),'LineWidth',2);
 
-
-%B = im2bw(B, graythresh(B));
-%figure; imshow(B);
-%text(5,30,strcat('\color{red}Sum of dots:',num2str(length(B))))
-
-%LB = 20;
-%UB = 50;
-%Iout = xor(bwareaopen(bw,LB),  bwareaopen(bw,UB));
-%figure, imshow(Iout); title('Iout ');
-
-
-%figure; imshow(B);
-
-%se = strel('disk',3);
-%vizszintes=imdilate(Iout,se);
-
-%vizszintes=imerode(vizszintes,se);
-%vizszintes=imerode(vizszintes,se);
-%vizszintes=imclose(vizszintes,se);
-
-%vizszintes = bwareaopen(vizszintes, 100);
-%figure, imshow(vizszintes);title('Motherfuck ');
-%B = bwboundaries(vizszintes);
-%figure, imshow(vizszintes); title('Number of dots on the upper surface  ');
-
-%text(5,30,strcat('\color{red}Sum of dots:',num2str(length(B) - 2)))
-
+  %randomize text position for better visibility
+  rndRow = ceil(length(boundary)/(mod(rand*k,7)+1));
+  col = boundary(rndRow,2); row = boundary(rndRow,1);
+  h = text(col+1, row-1, num2str(L(row,col)));
+  set(h,'Color',colors(cidx),'FontSize',14,'FontWeight','bold');
+end
+text(5,30,strcat('\color{red}Value of dices:',num2str(numberOfObject)))
